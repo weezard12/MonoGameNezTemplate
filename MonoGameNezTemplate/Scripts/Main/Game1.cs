b7fd3f5ff5b618088814184c697c8576c060f250
weezard12 <weezard1234@gmail.com>
2024-09-21 11:16:28 +0300

    Add project files.

---
+++ MonoGameNezTemplate/Scripts/Main/Game1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+using Nez.ImGuiTools;
+
+namespace MonoGameNezTemplate.Scripts
+{
+    public class Game1 : Core
+    {
+        public Game1()
+        {
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            PauseOnFocusLost = false;
+            Core.ExitOnEscapeKeypress = false;
+
+            //no fps limit
+            IsFixedTimeStep = false;
+            Screen.SynchronizeWithVerticalRetrace = false;
+            Screen.ApplyChanges();
+
+            base.Initialize();
+
+            Core.DebugRenderEnabled = true;
+
+            var imGuiManager = new ImGuiManager();
+            Core.RegisterGlobalManager(imGuiManager);
+
+            // toggle ImGui rendering on/off. It starts out enabled.
+            imGuiManager.SetEnabled(true);
+        }
+
+    }
+}
---
+++ MonoGameNezTemplate/Scripts/Main/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new MonoGameNezTemplate.Scripts.Game1();
+game.Run();
